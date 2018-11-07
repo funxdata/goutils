89417add0c74a0007ccfda3d6f822d28dcec4b57
Chuanjian Wang <me@ckeyer.com>
2018-11-07 13:35:50 +0800

    fix hash

--- crypto/hash.go
+++ crypto/hash.go
@@ -8,20 +8,31 @@ import (
 	"os"
 )
 
-func HashFiles(h hash.Hash, files ...string) (string, error) {
-	if len(files) == 0 {
-		return "", fmt.Errorf("required at least one file.")
+func HashBytes(h hash.Hash, bs []byte) (string, int64, error) {
+	n, err := h.Write(bs)
+	if err != nil {
+		return "", 0, err
 	}
+	return hex.EncodeToString(h.Sum(nil)), int64(n), nil
+}
+
+func HashFiles(h hash.Hash, files ...string) (string, int64, error) {
+	if len(files) == 0 {
+		return "", 0, fmt.Errorf("required at least one file.")
+	}
+	var count int64
 	for _, v := range files {
 		f, err := os.Open(v)
 		if err != nil {
-			return "", err
+			return "", 0, err
 		}
 
-		if _, err := io.Copy(h, f); err != nil {
-			return "", fmt.Errorf("read %s failed, %s", f.Name(), err)
+		if n, err := io.Copy(h, f); err != nil {
+			return "", 0, fmt.Errorf("read %s failed, %s", f.Name(), err)
+		} else {
+			count += n
 		}
 	}
 
-	return hex.EncodeToString(h.Sum(nil)), nil
+	return hex.EncodeToString(h.Sum(nil)), count, nil
 }
